d4ea13f403c91ad25468095fb951dd8c2c8dc15f
Joe Lombrozo <joe@djeebus.net>
2019-07-03 20:31:26 -0400

    added more stuff

--- app.py
+++ app.py
@@ -1,31 +1,60 @@
+import flask
+import flask_assets
+import logging
 import requests
 
+logger = logging.getLogger('apartment-search')
+logging.basicConfig(level=logging.DEBUG)
 
-from flask import Flask, request, Response
+bundles = {
+    'app_js': flask_assets.Bundle(
+        'js/bootstrap.js',
+        'apartment-search.js',
+        output='bundles/app.js',
+    ),
+    'app_css': flask_assets.Bundle(
+        'css/bootstrap.css',
+        'apartment-search.css',
+        output='bundles/app.css',
+    )
+}
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+assets = flask_assets.Environment(app)
+assets.register(bundles)
+
+
+def _make_request(url):
+    query = flask.request.args
+    logger.info('api call: %s [%s]' % (url, query))
+    response = requests.get(url, query)
+    data = response.json()
+    return data
 
 
 @app.route('/zillow')
 def zillow():
-    response = requests.get('http://www.zillow.com/search/GetResults.htm?' + request.query_string.decode('ascii'))
+    zillow_url = 'http://www.zillow.com/search/GetResults.htm'
 
-    return Response(
-        response=response.content,
-        status=200,
-        mimetype='application/json',
-    )
+    response = _make_request(zillow_url, headers={
+        'Cookie':
+    })
+
+    return flask.jsonify(response)
 
 
 @app.route('/yelp')
 def yelp():
-    response = requests.get('http://www.yelp.com/search/snippet' + request.query_string.decode('ascii'))
+    yelp_url = 'http://www.yelp.com/search/snippet'
 
-    return Response(
-        response=response.content,
-        status=200,
-        mimetype='application/json',
-    )
+    response = _make_request(yelp_url)
+
+    return flask.jsonify(response)
+
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
 
 
 if __name__ == "__main__":
